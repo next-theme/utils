4dc5f74194ebaf192ca13b7e48480df718436726
Mimi <1119186082@qq.com>
2020-08-14 12:33:34 +0800

    Class syntax

--- index.js
+++ index.js
@@ -5,18 +5,25 @@ const fs = require('fs');
 const path = require('path');
 const { merge } = require('lodash');
 
-module.exports = function(hexo, pluginDir) {
-  this.hexo = hexo;
-  this.pluginDir = pluginDir;
-  this.getFilePath = function(file) {
+class Util {
+  constructor(hexo, pluginDir) {
+    this.hexo = hexo;
+    this.pluginDir = pluginDir;
+  }
+
+  getFilePath(file) {
     return this.pluginDir ? path.resolve(this.pluginDir, file) : file;
-  };
-  this.getFileContent = function(file) {
+  }
+
+  getFileContent(file) {
     return fs.readFileSync(this.getFilePath(file), 'utf8');
-  };
-  this.defaultConfigFile = function(key, file) {
+  }
+
+  defaultConfigFile(key, file) {
     const defaultConfig = file ? yaml.safeLoad(this.getFileContent(file)) : {};
     this.hexo.config[key] = merge(defaultConfig[key], this.hexo.theme.config[key], this.hexo.config[key]);
     return this.hexo.config[key];
-  };
-};
+  }
+}
+
+module.exports = Util;
