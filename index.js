3161bf370419549420495066e2ab4c0417f43fd9
Mimi <1119186082@qq.com>
2021-05-27 01:25:44 +0800

    Drop lodash.merge

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const yaml = require('js-yaml');
 const fs = require('fs');
 const path = require('path');
-const { merge } = require('lodash');
+const { deepMerge } = require('hexo-util');
 
 class Util {
   constructor(hexo, pluginDir) {
@@ -21,7 +21,7 @@ class Util {
 
   defaultConfigFile(key, file) {
     const defaultConfig = file ? yaml.load(this.getFileContent(file)) : {};
-    this.hexo.config[key] = merge(defaultConfig[key], this.hexo.theme.config[key], this.hexo.config[key]);
+    this.hexo.config[key] = deepMerge(defaultConfig[key], deepMerge(this.hexo.theme.config[key] || {}, this.hexo.config[key] || {}));
     return this.hexo.config[key];
   }
 }
