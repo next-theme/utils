5d46581caca37b6a4d7738c9e8780586f0eba138
Mimi <1119186082@qq.com>
2026-01-07 11:01:04 +0800

    Update deps

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const yaml = require('js-yaml');
+const { parse } = require('yaml');
 const fs = require('fs');
 const path = require('path');
 const { deepMerge } = require('hexo-util');
@@ -20,7 +20,7 @@ class Util {
   }
 
   defaultConfigFile(key, file) {
-    const defaultConfig = file ? yaml.load(this.getFileContent(file)) : {};
+    const defaultConfig = file ? parse(this.getFileContent(file)) : {};
     this.hexo.config[key] = deepMerge(defaultConfig[key], deepMerge(this.hexo.theme.config[key] || {}, this.hexo.config[key] || {}));
     return this.hexo.config[key];
   }
